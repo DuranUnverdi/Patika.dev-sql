347f1ad73d090d0a522c2088f2d9c3fc0c68c117
Duran Ünverdi <75857104+DuranUnverdi@users.noreply.github.com>
2022-06-06 20:45:42 +0300

    Create odev1.sql

---
+++ odev1.sql
@@ -0,0 +1,5 @@
+select title,description from film;
+select * from film where length between 60 and 75;
+select * from film where rental_rate= 0.99 and replacement_cost= 12.99 or replacement_cost= 28.99;
+select last_name from customer where first_name='mary';
+select * from film where length<50 and rental_rate!= 2.99 and rental_rate!= 4.99;
