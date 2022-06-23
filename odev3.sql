94698433b3574302a3f729111f2d67d49844091a
Duran Ünverdi <75857104+DuranUnverdi@users.noreply.github.com>
2022-06-23 12:50:19 +0300

    Create odev3.sql
    LIKE,ILIKE,AND,OR

---
+++ odev3.sql
@@ -0,0 +1,8 @@
+1)country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+       select Country from Country where Country like 'A%a'
+2)country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+       select Country from Country where Country like '______%n'
+3)film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+        select title from film WHERE title ilike '%t%t%t%t%'
+4)film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+        select * from film where title like 'C%' and length>90 and rental_rate=2.99 
